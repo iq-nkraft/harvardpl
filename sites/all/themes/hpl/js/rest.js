5785335c27fc5d4372d2b7237fa36d2128775676
iq-nkraft <nathaniel.kraft@gmail.com>
2016-08-16 10:47:29 -0400

    meh

--- sites/all/themes/hpl/js/rest.js
+++ sites/all/themes/hpl/js/rest.js
@@ -1,3 +1,17 @@
 /**
  * Created by nkraft on 8/15/2016.
  */
+$(document).ready(function() {
+    $.ajax({
+        url: "http://booksite-app.appspot.com/api/newsletter/7419/CNL2/current",
+        type: "GET",
+        dataType: 'jsonp',
+        headers: { "x-api-key": "2143ef40e718a8364a3704a15f6bb3441eb4030fa5bc8c8d58034a65aaaf01c6" }
+    }).then(function(data) {
+        // $('.greeting-id').append(data.id);
+        $('.greeting-id').append(data);
+        // $('.greeting-content').append(data.content);
+    });
+});
+
+
